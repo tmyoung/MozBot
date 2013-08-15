059873a7f0d1cb8c9cd854bb9a2c5740df3b1e9e
Thomas Farrow <moztaddle@gmail.com>
2013-08-15 11:00:31 +0100

    Connects to IRC

---
+++ connect.py
@@ -0,0 +1,27 @@
+import sys
+import socket
+import string
+
+HOST="irc.mozilla.org"
+PORT=6667
+NICK="Mozbot"
+IDENT="Mozbot"
+REALNAME="Test"
+readbuffer=""
+
+s=socket.socket( )
+s.connect((HOST, PORT))
+s.send("NICK %s\r\n" % NICK)
+s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+
+while 1:
+    readbuffer=readbuffer+s.recv(1024)
+    temp=string.split(readbuffer, "\n")
+    readbuffer=temp.pop( )
+
+    for line in temp:
+        line=string.rstrip(line)
+        line=string.split(line)
+
+        if(line[0]=="PING"):
+            s.send("PONG %s\r\n" % line[1])
